35b15cec840515c6a02b77ef0ce601ed36825a4d
duconja <zamahajev.dusan@gmail.com>
2019-01-11 06:30:30 +0100

    Finished communication

---
+++ Documents/WebAPI2/Client/MainWindow.xaml.cs
@@ -0,0 +1,102 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Client
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<string> messages = new List<string>();
+        private ConnectionFactory connectionFactory;
+        private IConnection connection;
+        private IModel channel;
+        private EventingBasicConsumer consumer = null;
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitListener("0");
+        }
+
+        private void Btn_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Works");
+        }
+
+        private void Consumer_Received(object sender, BasicDeliverEventArgs args)
+        {
+            var body = args.Body;
+            var message = Encoding.UTF8.GetString(body);
+            var routingKey = args.RoutingKey;
+            messages.Add(DateTime.Now.ToShortTimeString() + " -> " + message);
+            Dispatcher.Invoke(() =>
+            {
+                logs.Items.Clear();
+                foreach(var m in messages)
+                {
+                    logs.Items.Add(m);
+                }
+            });
+        }
+
+        private void InitListener(String route)
+        {
+            connectionFactory = new ConnectionFactory() { HostName = "localhost" };
+            connection = connectionFactory.CreateConnection();
+            channel = connection.CreateModel();
+            channel.ExchangeDeclare(exchange: "topic_logs", type: "topic");
+            var queueName = channel.QueueDeclare().QueueName;
+
+
+            channel.QueueBind(queue: queueName,
+                              exchange: "topic_logs",
+                              routingKey: route);
+
+
+            consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) => Consumer_Received(model, ea);
+            //{
+            //    var body = ea.Body;
+            //    var message = Encoding.UTF8.GetString(body);
+            //    var routingKey = ea.RoutingKey;
+            //    messages.Add(DateTime.Now.ToShortTimeString() + " -> " + message);
+            //    logs.Items.Add(message);
+            //    logs.Items.Refresh();
+
+            //};
+            channel.BasicConsume(queue: queueName,
+                                 autoAck: true,
+                                 consumer: consumer);
+
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (channel.IsOpen)
+                channel.Close();
+            if (connection.IsOpen)
+                connection.Close();
+        }
+
+        private void BtnRoute_Click(object sender, RoutedEventArgs e)
+        {
+
+            InitListener(txtRoute.Text);
+        }
+    }
+}
---
+++ Documents/WebAPI2/Server/MainWindow.xaml.cs
@@ -0,0 +1,94 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Server
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Cimey cimey;
+        public MainWindow()
+        {
+            InitializeComponent();
+            cimey = new Cimey() { Name = "Dacha Pogacha", Age = 22, Avg = 9.17 };
+        }
+
+        private void Btn_send_Click(object sender, RoutedEventArgs e)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(exchange: "topic_logs",
+                                        type: "topic");
+
+                var routingKey = "1";
+
+                string send_string = "";
+
+                foreach (object grid in LogicalTreeHelper.GetChildren(this))
+                {
+                    foreach (object control in LogicalTreeHelper.GetChildren((Grid)grid))
+                    {
+                        if (control is RadioButton)
+                        {
+                            RadioButton rb = control as RadioButton;
+                            if (rb.IsChecked == true)
+                            {
+                                send_string += (control as RadioButton).Content.ToString() + " ";
+                                if (rb.Name == "rbtn_Inform")
+                                    send_string += tbx_Information.Text + " ";
+                            }
+                        }
+                    }
+                }
+                var body = Encoding.UTF8.GetBytes(send_string);
+                channel.BasicPublish(exchange: "topic_logs",
+                                     routingKey: routingKey,
+                                     basicProperties: null,
+                                     body: body);
+            }
+
+        }
+
+        private void BtnSendCimey_Click(object sender, RoutedEventArgs e)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(exchange: "topic_logs",
+                                        type: "topic");
+
+                var routingKey = "2";
+
+                string send_string = "";
+
+                send_string = JsonConvert.SerializeObject(cimey);
+                
+                var body = Encoding.UTF8.GetBytes(send_string);
+                channel.BasicPublish(exchange: "topic_logs",
+                                     routingKey: routingKey,
+                                     basicProperties: null,
+                                     body: body);
+            }
+
+        }
+    }
+}
