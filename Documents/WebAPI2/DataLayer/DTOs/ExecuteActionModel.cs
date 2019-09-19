2bb7e8a08539a8e55407a5483cf272d5d1f3e5d1
duconja <zamahajev.dusan@gmail.com>
2019-09-19 04:18:42 +0200

    large push

---
+++ Documents/WebAPI2/DataLayer/DTOs/ExecuteActionModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.DTOs
+{
+    public class ExecuteActionModel
+    {
+        public int Who { get; set; }
+        public int To { get; set; }
+
+        public ExecuteActionModel()
+        {
+
+        }
+
+        public ExecuteActionModel(int who, int to)
+        {
+            Who = who;
+            To = to;
+        }
+    }
+}
